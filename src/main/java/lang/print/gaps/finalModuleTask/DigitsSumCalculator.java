9dce7b3a3801e1747b9d0fb00fbc2612819e54aa
Qodirov Lochinbek <lochinbek433@gmail.com>
2022-07-19 18:37:46 +0500

    Done!

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int first = number/1000, second = (number / 100) % 10, third = (number / 10) % 10, fourth = number % 10;
+        System.out.println(first + second + third + fourth);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -1,6 +1,8 @@
 package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
-    public void revert(int number) {
+    public void revert(int number) {//458
+        int first= number / 100,second = (number / 10) %10,third = number % 10;
+        System.out.println(third * 100 + second * 10 + first);
     }
 }
